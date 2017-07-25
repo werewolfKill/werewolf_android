d70031aff41174407a4ce47828930b57d94e8c9c
wangtonghe <wthfeng@126.com>
2017-07-26 00:57:01 +0800

    开始主页面若干按钮

--- app/src/main/java/com/zinglabs/zwerewolf/ui/HomeFragment.java
+++ app/src/main/java/com/zinglabs/zwerewolf/ui/HomeFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.zinglabs.zwerewolf.R;
 
@@ -44,24 +45,37 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         root.findViewById(R.id.home_easy_iv).setOnClickListener(this);
 
         root.findViewById(R.id.home_help_iv).setOnClickListener(this);
+        root.findViewById(R.id.home_search_iv).setOnClickListener(this);
+        root.findViewById(R.id.home_standard_iv).setOnClickListener(this);
+        root.findViewById(R.id.home_create_iv).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.home_easy_iv:
+            case R.id.home_easy_iv:  //简单模式
                 easy();
                 break;
-            case R.id.menu_home_setting:
-                //showToast("setting");
+            case R.id.home_standard_iv: //标准模式
+                showToast("暂未开发,敬请期待...");
                 break;
-            case R.id.home_help_iv:
-                //showToast("help");
+            case R.id.menu_home_setting:  //设置
+                showToast("暂未开发...");
                 break;
+            case R.id.home_help_iv:   //帮助
+                showToast("暂未开发...");
+                break;
+            case R.id.home_search_iv:  //搜索房间
+                showToast("暂未开发...");
+                break;
+            case R.id.home_create_iv:  //创建房间
+                createRoom();
+                break;
+
         }
     }
 
-    public void easy() {
+    private void easy() {
      //加入房间之前的参数
       String channel = "wzc25151";
      //  App.mAudioSettings.mChannelName = channel;
@@ -69,5 +83,17 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
        i.putExtra("ecHANEL", channel);
        startActivity(i);
         startActivity(new Intent(activity, GameActivity.class));
+    }
+
+    private void createRoom(){
+        showToast("暂未开发...");
+        System.out.println("创建房间");
+
+    }
+
+    private void showToast(String str){
+
+        Toast.makeText(activity,str,Toast.LENGTH_SHORT).show();
+
     }
 }
